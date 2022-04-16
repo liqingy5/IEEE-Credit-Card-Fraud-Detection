09f47dea3fcfe378ded2a8385d5f1fd4d231e69b
Shibo Xing <shx26@pitt.edu>
2022-04-15 21:51:50 -0400

    feat: add test to missing values, add DNN evaluation

---
+++ AlexNet.py
@@ -0,0 +1,39 @@
+from torch import nn
+import torch
+
+class AlexNet(torch.nn.Module):
+    def __init__(self, num_classes: int = 2, dropout: float = 0.5) -> None:
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv1d(1, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=3, stride=2),
+            nn.Conv1d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=3, stride=2),
+            nn.Conv1d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool1d(6)
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Linear(256 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        x = torch.flatten(x, 0)
+        return x
